d1b6418a9587daa3ba076f885e07adf6e5950004
gleb.sturov <gsturov@ca.ibm.com>
2020-03-20 11:01:25 -0400

    separate framework from reference connector

---
+++ base_import.py
@@ -0,0 +1,36 @@
+from car_framework.util import check_for_error, BATCH_SIZE
+from car_framework.context import context
+
+class BaseImport(object):
+
+    def __init__(self):
+        self.statuses = []
+
+
+    def create_source_report_object(self):
+        raise NotImplementedError()
+
+
+    def wait_for_completion_of_import_jobs(self):
+        context().car_service.check_import_status(self.statuses)
+        for status in self.statuses:
+            check_for_error(status)
+        self.statuses = []
+
+
+    def send_data(self, name, data):
+        envelope = self.create_source_report_object()
+        envelope[name] = data
+        status = context().car_service.import_data(envelope)
+        check_for_error(status)
+        self.statuses.append(status)
+        if len(self.statuses) == BATCH_SIZE:
+            self.wait_for_completion_of_import_jobs()
+
+
+    def get_last_model_state_id(self):
+        return context().car_service.get_model_state_id()
+
+
+    def save_new_model_state_id(self, new_model_state_id):
+        return context().car_service.save_model_state_id(new_model_state_id)
