78d72240ec4e01a938a879a5b91adc2d2b7c78d7
Arthur Muradyan <arthur.muradyan@ibm.com>
2020-11-04 11:39:33 -0400

    Travis test

--- publish.sh
+++ publish.sh
@@ -3,9 +3,7 @@ function log()
    echo $(date -u) "[$LOG_PREFIX]: $1"
 }
 
-echo "Publishing:"
-date -u
-
+log "Staring Publish"
 
 VERSION_LAST_TAG=$(git describe --abbrev=0 --tags 2>/dev/null)
 
@@ -19,11 +17,12 @@ else
     export PYPI_PACKAGE_VERSION=${VERSION_LAST_TAG}
 fi
 
-echo "TRAVIS_TAG: ${TRAVIS_TAG}"
-echo "PYPI_PACKAGE_VERSION: ${PYPI_PACKAGE_VERSION}"
+log "TRAVIS_TAG: ${TRAVIS_TAG}"
+log "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
+log "PYPI_PACKAGE_VERSION: ${PYPI_PACKAGE_VERSION}"
 
 if [ "${TO_PUBLISH}" == "true" ] ; then
-    echo "TO_PUBLISH is true"
+    log "TO_PUBLISH is true"
 
     pip install setuptools wheel twine
 
@@ -31,6 +30,6 @@ if [ "${TO_PUBLISH}" == "true" ] ; then
 
     python setup.py sdist bdist_wheel
 
-    # python3 -m twine upload -u "__token__" -p "<TOKEN>" --repository testpypi dist/*
+    # python3 -m twine upload -u "__token__" -p "${PYPI_API_TOKEN}" --repository testpypi dist/*
 
 fi
