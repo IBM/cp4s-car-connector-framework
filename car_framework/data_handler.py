dabc59caf257eff9e41366eb368e1db514eb97f6
Gleb Sturov <gsturov@ca.ibm.com>
2022-06-16 11:54:14 -0700

    adopt GraphQL mutations for importing data

--- car_framework/data_handler.py
+++ car_framework/data_handler.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import json
+import jsonpickle
 import os
 import shutil
 import uuid
@@ -8,10 +9,63 @@ from car_framework.context import context
 from car_framework.full_import import BaseFullImport
 
 
-def get_report_time():
-    delta = datetime.utcnow() - datetime(1970, 1, 1)
-    milliseconds = delta.total_seconds() * 1000
-    return milliseconds
+class JsonField():
+    def __init__(self, obj):
+        self.obj = obj
+
+
+class Mutation():
+    def __init__(self, collection_name, data):
+        self.collection_name = collection_name
+        self.data = data
+        self.var_count = 0
+        self.vars = {}
+
+
+    def save(self, file_path):
+        data = jsonpickle.encode(self)
+        with open(file_path, 'w') as outfile:
+            outfile.write(data)
+
+
+    @staticmethod
+    def load(file_path):
+        with open(file_path, 'r') as inpfile:
+            data = inpfile.read()
+            mutation = jsonpickle.decode(data)
+            return mutation
+
+
+    def serialize(self):
+        body = '''
+            {
+                insert_%s(objects: [ %s ]) { affected_rows }
+            }
+        ''' % (self.collection_name, ',\n'.join( map(lambda item: self._serialize_object(item), self.data) ))
+
+        args = ''
+        if self.vars: args = '(%s)' % ', '.join( map(lambda var: f'${var}: jsonb', self.vars.keys()) )
+
+        query = 'mutation %s %s' % (args, body)
+        if self.vars: return {'query': query, 'variables': self.vars}
+        else: return {'query': query}
+
+
+    def _serialize_object(self, obj):
+        return '{%s}' % ', '.join(map(lambda item: self._serialize_field(item[0], item[1]), obj.items()))
+
+
+    def _serialize_field(self, name, value):
+        if isinstance(value, JsonField): return '%s: %s' % (name, self._serialize_json_field(value))
+        elif type(value) == int or type(value) == float: return '%s: %s' % (name, value)
+        else: return '%s: "%s"' % (name, str(value))
+
+
+    def _serialize_json_field(self, value):
+        self.var_count += 1
+        self.vars['var%d' % self.var_count] = value.obj
+        return '$var%d' % self.var_count
+
 
 
 class BaseDataHandler():
@@ -25,14 +79,10 @@ class BaseDataHandler():
     edge_keys = {}
 
     def __init__(self):
-        self.timestamp = get_report_time()
         self.export_data_dir = os.path.join(context().args.export_data_dir, datetime.now().strftime('%Y-%m-%d_%H:%M:%S_r%f'))
 
-    def create_source_report_object(self):
-        raise NotImplementedError()
-
-        # Adds the collection data
-    def add_collection(self, name, object, key):
+    # Adds the collection data
+    def add_item_to_collection(self, name, object):
         objects = self.collections.get(name)
         if not objects:
             objects = []
@@ -43,9 +93,9 @@ class BaseDataHandler():
             keys = []
             self.collection_keys[name] = keys
 
-        if not object[key] in self.collection_keys[name]:
+        if not object['external_id'] in self.collection_keys[name]:
             objects.append(object)
-            self.collection_keys[name].append(object[key])
+            self.collection_keys[name].append(object['external_id'])
 
         # dump collection to file to free memory
         if len(self.collections[name]) >= context().args.export_data_page_size:
@@ -67,10 +117,8 @@ class BaseDataHandler():
 
         key = '#'.join(str(x) for x in object.values())
         if not key in self.edge_keys[name]:
-            object['report'] = self.report['_key']
             object['source'] = context().args.source
-            object['active'] = True
-            object['timestamp'] = self.report['timestamp']
+            object['reported_at'] = context().report_time
             objects.append(object)
             self.edge_keys[name].append(key)
 
@@ -81,8 +129,6 @@ class BaseDataHandler():
 
     def send_collections(self, importer):
         context().logger.info('Creating vertices')
-        if not self.collections and isinstance(importer, BaseFullImport):
-            self._send_empty_report(importer)
         for name, data in self.collections.items():
             # save residual data
             if len(data) > 0:
@@ -92,17 +138,12 @@ class BaseDataHandler():
 
     def send_edges(self, importer):
         context().logger.info('Creating edges')
-        if not self.edges and isinstance(importer, BaseFullImport):
-            self._send_empty_report(importer)
         for name, data in self.edges.items():
             # save residual data
             if len(data) > 0:
                 self._save_export_data_file(name, data)
             self._send(name, importer)
         context().logger.info('Creating edges done: %s', {key: len(value) for key, value in self.edge_keys.items()})
-
-    def _send_empty_report(self, iporter):
-        iporter.send_data(None, None)
 
     def _create_export_data_dir(self, name):
         dir_path = os.path.join(self.export_data_dir, name)
@@ -119,19 +160,17 @@ class BaseDataHandler():
 
     def _save_export_data_file(self, name, data):
         dir_path = self._create_export_data_dir(name)
-        envelope = self.create_source_report_object()
-        envelope[name] = data
         filename = os.path.join(dir_path, '%s.json' % str(uuid.uuid4())[0:8])
-        with open(filename, 'w') as outfile:
-            json.dump(envelope, outfile)
+        mutation = Mutation(name, data)
+        mutation.save(filename)
         return filename
 
     def _send(self, name, importer):
         dir_path = os.path.join(self.export_data_dir, name)
         for _, _, files in os.walk(dir_path):
             for data_file in files:
-                with open(os.path.join(dir_path, data_file), 'r') as outfile:
-                    importer.send_data_from_file(name, outfile)
+                mutation = Mutation.load(os.path.join(dir_path, data_file))
+                importer.send_mutation(mutation)
         self._delete_export_data_dir(dir_path)
 
     def printData(self):
--- car_framework/inc_import.py
+++ car_framework/inc_import.py
@@ -1,12 +1,12 @@
 from car_framework.base_import import BaseImport
 from car_framework.context import context
-from car_framework.util import IncrementalImportNotPossible, RecoverableFailure
-from car_framework.car_service import CarDbStatus
+from car_framework.util import IncrementalImportNotPossible
 
 
 class BaseIncrementalImport(BaseImport):
     def __init__(self):
         super().__init__()
+        self.updated_vertices = {}
 
 
     def get_new_model_state_id(self):
@@ -29,13 +29,27 @@ class BaseIncrementalImport(BaseImport):
         raise NotImplementedError()
 
 
-    def run(self):
-        db_status = context().car_service.get_db_status()
-        if db_status == CarDbStatus.FAILURE:
-            raise RecoverableFailure('Database is not ready.')
-        if db_status == CarDbStatus.NEWLY_CREATED:
-            raise IncrementalImportNotPossible('Newly created CAR database is detected.')
+    def get_owned_edges(collection):
+        raise NotImplementedError()
 
+
+    def add_updated_vertex(self, collection, id):
+        ids = self.updated_vertices.get(collection)
+        if ids == None:
+            ids = []
+            self.updated_vertices[collection] = ids
+        ids.append(id)
+
+
+    def limit_edges_of_updated_vertices_to_current_report(self):
+        for collection in self.updated_vertices.keys():
+            edge_collections = self.get_owned_edges(collection)
+            if edge_collections:
+                context().car_service.limit_edges_to_report(context().args.source, collection, edge_collections, self.updated_vertices[collection], context().report_time)
+
+
+    def run(self):
+        context().car_service.create_source_if_needed()
         last_model_state_id = self.get_last_model_state_id()
         if not last_model_state_id:
             raise IncrementalImportNotPossible('"Last known model state" is not available.')
@@ -44,11 +58,16 @@ class BaseIncrementalImport(BaseImport):
         if not new_model_state_id:
             raise IncrementalImportNotPossible('Current model state is not available.')
 
+        if last_model_state_id == new_model_state_id:
+            context().logger.info('The source model has not changed.')
+            return
+
+        context().car_service.prepare_incremental_import(context().report_time)
         self.get_data_for_delta(last_model_state_id, new_model_state_id)
         self.import_vertices()
-        self.wait_for_completion_of_import_jobs()
         self.import_edges()
-        self.wait_for_completion_of_import_jobs()
+        self.limit_edges_of_updated_vertices_to_current_report()
         self.delete_vertices()
+        context().car_service.complete_incremental_import()
 
         self.save_new_model_state_id(new_model_state_id)
