7570d2ffce96ca812ad18bece8b6bb55ebcac195
Omkar Gurav <omkargurav@omkars-mbp-2.frd.can.ibm.com>
2023-11-14 09:04:00 -0400

    Debug

--- publish.sh
+++ publish.sh
@@ -20,9 +20,7 @@ else
     EFFECTIVE_BRANCH="${TRAVIS_PULL_REQUEST_BRANCH}"
 fi
 
-echo "PYPI repo ${{ vars.PYPI_API_REPOSITORY_PROD }}"
-echo "PYPI repo ${{ env.PYPI_API_REPOSITORY_PROD }}"
-echo "PYPI repo ${{ github.PYPI_API_REPOSITORY_PROD }}"
+echo "PYPI repo ${{ PYPI_API_REPOSITORY_PROD }}"
 # choose repository
 if [[ "${EFFECTIVE_BRANCH}" =~ ^(develop|master|prod-test-.*|v[0-9]+(\.[0-9]+){0,4})$ ]]; then
     PYPI_API_REPOSITORY="${vars.PYPI_API_REPOSITORY_PROD}"
